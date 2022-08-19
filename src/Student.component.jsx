69e07a742b90af01c8708fde433a62336fa6b592
DanielMalede <danielmalede@gmail.com>
2022-08-19 22:16:59 +0300

    .

--- src/Student.component.jsx
+++ src/Student.component.jsx
@@ -3,6 +3,7 @@ export default function Student() {
     let [student,setStudent]=useState({F_name:"N/A",L_name:"N/A",Age:"N/A"})
     return(
         <div>
+            
         </div>
     )
     
