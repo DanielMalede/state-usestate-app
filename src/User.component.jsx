32d53166fdb22162a18fce6afbb8c551452541ac
DanielMalede <danielmalede@gmail.com>
2022-08-21 21:50:26 +0300

    .

--- src/User.component.jsx
+++ src/User.component.jsx
@@ -1,10 +1,10 @@
-import AddUser from "./AddUser.component"
-import PrintUserTable from './UseTable.component'
-export default function User() {
-    return(
-        <div>
-        <AddUser firstname="Danil" lName="Malede" Age="25" Email="daniel@gmail.com" Password="123456"/>
-        <PrintUserTable/>
-        </div>
-    )
-}
+// import AddUser from "./AddUser.component"
+// import PrintUserTable from './UseTable.component'
+// export default function User() {
+//     return(
+//         <div>
+//         <AddUser firstname="Danil" lName="Malede" Age="25" Email="daniel@gmail.com" Password="123456"/>
+//         <PrintUserTable/>
+//         </div>
+//     )
+// }
--- src/course.component.jsx
+++ src/course.component.jsx
@@ -7,6 +7,9 @@ export default function CourseFun() {
     let [teacher,setTeacher]= useState({F_name:"N/A",L_name:"N/A"}) 
     const ChangeStudentArray =()=>setStudent((stusent)=>{return[...stusent,{F_name:"Daniel",L_name:"Malede",Age:25},{F_name:"Lior",L_name:"David",Age:26},{F_name:"Or",L_name:"Gedamo",Age:24},{F_name:"Lior",L_name:"Dawwit",Age:26}]})
     const addNewTOSubj = ()=>setarraySubj((arraySubj)=>{return[...arraySubj,"New"]})
+    const getValueOfF_name = (e)=>{setStudent(e.target.value)}
+    const addF_name=()=>setStudent([...ChangeStudentArray])
+    const hendlerForm=(e)=>e.preventDefault()
     return(
         <div>
             {arraySubj.map((item)=><p>{item}</p>)}
@@ -15,7 +18,11 @@ export default function CourseFun() {
             <button onClick={ChangeStudentArray}>add to student array</button>
             <br/>
             <button onClick={addNewTOSubj}>add "New" to subj</button>
-            {stusent.map(()=><Student/>)}
+            {/* {stusent.map(()=><Student/>)} */}
+            <form onSubmit={hendlerForm}>
+                <input type="text"   onChange={(e)=>getValueOfF_name(e)}/>
+                <button onClick={addF_name}>add name</button>
+            </form>
         </div>
     ) 
 }
